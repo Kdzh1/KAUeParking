832addc065125266141f1b0e77c04f4f1966e254
Azooz77a <97988789+Azooz77a@users.noreply.github.com>
2022-04-09 05:09:25 +0300

    النصر

--- app/src/main/java/com/example/kaueparking/DBHelper.java
+++ app/src/main/java/com/example/kaueparking/DBHelper.java
@@ -13,7 +13,7 @@ public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(Context context) {
         super(context, "DB2022", null, 1);
     }
-
+// يا عيال وين برزنتيشن خديدوووووووووووووووووووووووووووووووس
     @Override
     public void onCreate(SQLiteDatabase DB) {
         DB.execSQL("CREATE TABLE admin (id TEXT UNIQUE, password TEXT,name TEXT,phone TEXT, PRIMARY KEY(id))");
