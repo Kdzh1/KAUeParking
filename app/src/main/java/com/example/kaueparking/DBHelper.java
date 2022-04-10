d3afbc1a25653cf889cbc477e3c4cd9b9e2744e8
khali <khalidzm99@gmail.com>
2022-04-10 19:56:59 +0300

    resolving the errors

--- app/src/main/java/com/example/kaueparking/DBHelper.java
+++ app/src/main/java/com/example/kaueparking/DBHelper.java
@@ -19,8 +19,9 @@ public class DBHelper extends SQLiteOpenHelper {
         DB.execSQL("CREATE TABLE admin (id TEXT UNIQUE, password TEXT,name TEXT,phone TEXT, PRIMARY KEY(id))");
         DB.execSQL("CREATE TABLE driver (id TEXT UNIQUE, password TEXT, name TEXT, phone TEXT , PRIMARY KEY(id));");
         DB.execSQL("CREATE TABLE security (id TEXT UNIQUE, password TEXT, name TEXT, phone TEXT, PRIMARY KEY(id));");
-        DB.execSQL("CREATE TABLE ticket (id INTEGER UNIQUE,plate TEXT,price TEXT,location TEXT, time TEXT, status INTEGER DEFAULT 0, approved INTEGER DEFAULT 0, driverID TEXT, PRIMARY KEY(id), FOREIGN KEY(driverID)REFERENCES driver (id));");
-
+        DB.execSQL("CREATE TABLE ticket (id INTEGER UNIQUE,plate TEXT,price TEXT,location TEXT, time TEXT, status INTEGER DEFAULT 0, approved INTEGER DEFAULT 1, driverID TEXT, violation BLOB, PRIMARY KEY(id), FOREIGN KEY(driverID)REFERENCES driver (id));");
+        DB.execSQL("insert into security values('1222','123','khalid','0546545654')");
+        DB.execSQL("insert into driver values ('9888','123','ahmed','65564654')");
     }
 
     @Override
