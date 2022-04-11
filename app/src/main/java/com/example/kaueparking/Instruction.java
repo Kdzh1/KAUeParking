08429dfcae6fdba49a4554126bc7a8fcd029a75d
azooz017aa <azooz.017uni@gmail.com>
2022-04-11 20:09:55 +0300

    I delet advice classes and create an instrucion.xml class

--- app/src/main/java/com/example/kaueparking/Instruction.java
+++ app/src/main/java/com/example/kaueparking/Instruction.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Advice extends AppCompatActivity {
+public class Instruction extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_advice);
+        setContentView(R.layout.instrucion);
     }
 }
--- app/src/main/java/com/example/kaueparking/SecurityActivity.java
+++ app/src/main/java/com/example/kaueparking/SecurityActivity.java
@@ -3,28 +3,64 @@ package com.example.kaueparking;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecurityActivity extends AppCompatActivity {
+
+    //imgBtn=Image Button , Btn=Button
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.securityhomepage);
-        TextView newFine = findViewById(R.id.userID);
+
+        // attributes in this class
+        ImageButton newFine = findViewById(R.id.addNewTktBtn); //the button of new ticket (its imgBtn not just Btn)
+        ImageButton instructionBtn =(ImageButton)findViewById(R.id.instructionBtn);
+        ImageButton infoBtn=(ImageButton)findViewById(R.id.infoBtn);
+        //
+
+        // Instructions
+        instructionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { openInstruction(); }
+        });
+        //
+
+        // Add new ticket
         newFine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openNewFine();
             }
         });
+        //
+
+        // My information
+        infoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) { openMyinformation(); }
+        });
+        //
+    }
+    public void openInstruction(){
+        Intent Insintent= new Intent(this, Instruction.class); // Instruction Intent
+        startActivity(Insintent);
     }
     public void openNewFine(){
 
-            Intent intent = new Intent(this, newFine.class);
-            startActivity(intent);
+            Intent Tktintent = new Intent(this, newFine.class); // Add new Ticket Intent
+            startActivity(Tktintent);
 
     }
+    public void openMyinformation(){
+
+        Intent Infointent = new Intent(this, myinformation.class); // Add new Ticket Intent
+        startActivity(Infointent);
+
+    }
+
 }
