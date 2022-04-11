17912fa285069ae267eef96ccacaeb47e309529f
khali <khalidzm99@gmail.com>
2022-04-11 06:29:40 +0300

    adding the violation picture

--- app/src/main/java/com/example/kaueparking/SecurityActivity.java
+++ app/src/main/java/com/example/kaueparking/SecurityActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,8 +12,8 @@ public class SecurityActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_security);
-        Button newFine = findViewById(R.id.userID);
+        setContentView(R.layout.securityhomepage);
+        TextView newFine = findViewById(R.id.userID);
         newFine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
