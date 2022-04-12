0a380c3a45bb8cb2c376b960d06f160e8518ce00
khali <khalidzm99@gmail.com>
2022-04-12 03:43:55 +0300

    inserting new Ticket complete

--- app/src/main/java/com/example/kaueparking/DBHelper.java
+++ app/src/main/java/com/example/kaueparking/DBHelper.java
@@ -24,6 +24,7 @@ public class DBHelper extends SQLiteOpenHelper {
         DB.execSQL("CREATE TABLE ticket (id INTEGER UNIQUE,plate TEXT,price TEXT,location TEXT, time TEXT, status INTEGER DEFAULT 0, approved INTEGER DEFAULT 1, driverID TEXT, violation BLOB, PRIMARY KEY(id), FOREIGN KEY(driverID)REFERENCES driver (id));");
         DB.execSQL("insert into security values('1222','123','khalid','0546545654')");
         DB.execSQL("insert into driver values ('9888','123','ahmed','65564654','2724EJD')");
+        DB.execSQL("insert into ticket values (0,'0','0','0','0',0,1,'0',0)");
     }
 
     @Override
