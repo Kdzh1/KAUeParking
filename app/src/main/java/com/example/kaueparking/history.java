60ade7c486ab500f33b45b16370d54491e9868ca
azooz017aa <azooz.017uni@gmail.com>
2022-04-12 09:03:49 +0300

    added activity_history.xml

--- app/src/main/java/com/example/kaueparking/history.java
+++ app/src/main/java/com/example/kaueparking/history.java
@@ -3,12 +3,17 @@ package com.example.kaueparking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class objection extends AppCompatActivity {
+public class history extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_objection);
+        setContentView(R.layout.activity_history);
+
+        Button bt=findViewById(R.id.obj);
+
+        bt.setText("done!");
     }
 }
