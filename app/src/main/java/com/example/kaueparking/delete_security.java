a10e6e21bd7e0eb094a7fdb9b4227ed5986aebbe
azooz017aa <azooz.017uni@gmail.com>
2022-05-12 01:03:36 +0300

    finish activity_admin_detailed_objection.xml + add activity_delete_security.xml and finish it

---
+++ app/src/main/java/com/example/kaueparking/delete_security.java
@@ -0,0 +1,14 @@
+package com.example.kaueparking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class delete_security extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_security);
+    }
+}
