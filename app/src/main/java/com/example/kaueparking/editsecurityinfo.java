75a6a491c661dd3a433260e17a8f046fa0030dfa
azooz017aa <azooz.017uni@gmail.com>
2022-05-13 02:38:59 +0300

    سويت اشياء كثير منها بعض ملاحظات الدكتور
    واضفت كل الكلاسات المطلوبة تحديدا حقت الادمن
    وعدلت بعض التصاميم

---
+++ app/src/main/java/com/example/kaueparking/editsecurityinfo.java
@@ -0,0 +1,13 @@
+package com.example.kaueparking;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class editsecurityinfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editsecurityinfo);
+    }
+}
