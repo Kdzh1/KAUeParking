3c9927a4dbaef8e7ea5cca26616bd53e6cacf6b7
khali <khalidzm99@gmail.com>
2022-05-16 07:58:39 +0300

    add methods ro DRIVER, ADMIN, SECURITY

--- app/src/main/java/com/example/kaueparking/register.java
+++ app/src/main/java/com/example/kaueparking/register.java
@@ -21,7 +21,6 @@ public class register extends AppCompatActivity {
         Button signUpBtn = findViewById(R.id.SignUp);
         TextView toSignIn = findViewById(R.id.toSignIn);
         DBHelper db = new DBHelper(this);
-        AlertDialog.Builder alert = new AlertDialog.Builder(this);
         toSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,22 +35,18 @@ public class register extends AppCompatActivity {
                 EditText name = findViewById(R.id.Name);
                 EditText pass = findViewById(R.id.Pass);
                 EditText phone = findViewById(R.id.Phone);
+                EditText id = findViewById(R.id.newID);
+                EditText plate = findViewById(R.id.newPlate);
                 d.setName(name.getText().toString());
                 d.setPassword(pass.getText().toString());
                 d.setPhone(phone.getText().toString());
+                d.setId(2+id.toString());
+                d.setPlate(plate.toString());
                 if (d.verifyInfo()) {
                 d.setId(db.getNewSecurityID());
-                alert.setTitle("New Security ID is: ");
-                alert.setMessage(db.getNewSecurityID());       // IN CASE OF WRONG PASS OR ID THEN POP UP AN ALERT
-                alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                    }
-                });
-                alert.create().show();
 
                     if (db.insertData(d)) {
-                        Toast.makeText(getApplicationContext(), "New security added successfully", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "Registered successfully", Toast.LENGTH_LONG).show();
                         finish();
                     } else {
                         Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_LONG).show();
